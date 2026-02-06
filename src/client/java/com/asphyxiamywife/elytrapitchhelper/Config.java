d09a1bbdac6ad429de1bf8f1e80ccc0b702596b0
asphyxiamywife <48881569+asphyxiamywife@users.noreply.github.com>
2026-02-06 01:16:09 +0100

    feat: config migration

--- src/client/java/com/asphyxiamywife/elytrapitchhelper/Config.java
+++ src/client/java/com/asphyxiamywife/elytrapitchhelper/Config.java
@@ -33,7 +33,11 @@ public final class Config {
         if (Files.exists(path)) {
             try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
                 Config cfg = GSON.fromJson(reader, Config.class);
-                return cfg != null ? cfg : createAndSaveDefault();
+                if (cfg != null) {
+                    cfg.save();
+                    return cfg;
+                }
+                return createAndSaveDefault();
             } catch (IOException e) {
                 return createAndSaveDefault();
             }
